e680db12f7ea40286e6200bd4b71d30875ae1396
Nick Guimarais <Nick.guimarais@gmail.com>
2023-10-31 21:12:10 -0700

    add animation

--- my-react-app/src/components/Accordian/Accordian.jsx
+++ my-react-app/src/components/Accordian/Accordian.jsx
@@ -1,28 +1,50 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { FiPlus, FiMinus } from "react-icons/fi";
 
 import styles from "./accordian.module.css";
 
 export default function Accordian({ title, text }) {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+  const [contentHeight, setContentHeight] = useState(0);
+  const [offSetHeight, setOffSetHeight] = useState(0);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setContentHeight(isOpen ? contentRef.current.scrollHeight : 0);
+      setOffSetHeight(isOpen ? contentRef.current.clientHeight : 0);
+    }
+  }, [isOpen]);
+
+  const containerStyle = {
+    maxHeight: "0px",
+    transform: `scaleY(${isOpen ? 1 : 0})`,
+    transformOrigin: "top",
+  };
+
+  const expandedStyle = {
+    minHeight: isOpen ? `${contentHeight + offSetHeight * 2}px` : "",
+  };
+
   return (
-    <div className={styles.container}>
+    <div
+      className={`${styles.container} `}
+      style={expandedStyle}
+      onClick={() => setIsOpen(!isOpen)}
+    >
       <div className={styles.flex}>
         <div className={styles.header}>
           <p>{title}</p>
           <div className={styles.iconWrapper}>
-            {isOpen && (
-              <FiMinus
-                className={styles.icon}
-                onClick={() => setIsOpen(false)}
-              />
-            )}
-            {!isOpen && (
-              <FiPlus className={styles.icon} onClick={() => setIsOpen(true)} />
-            )}
+            {isOpen && <FiMinus className={styles.icon} />}
+            {!isOpen && <FiPlus className={styles.icon} />}
           </div>
         </div>
-        {isOpen && <div className={styles.body}>{text}</div>}
+        {isOpen && (
+          <div ref={contentRef} className={styles.body} style={containerStyle}>
+            {text}
+          </div>
+        )}
       </div>
     </div>
   );
